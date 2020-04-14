fd2704d824662c17c90d8bd8376f17cf62f6053e
Marie Antons <maantons@mail.fhsu.edu>
2020-04-14 10:27:40 -0500

    fixed server.js bug

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 'use strict';
 
-
 require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'))
+// app.use(express.urlencoded({extended:true}));
 
 
 
-
+app.get('/', (request, response) => {
+    response.render('pages/index');
+});
 
 
 
