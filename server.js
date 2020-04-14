cfbaf1c02538cf532a079e9f13704ee3b2081fdf
Marie Antons <maantons@mail.fhsu.edu>
2020-04-14 09:58:31 -0500

    adding scaffolding, connecting to heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
